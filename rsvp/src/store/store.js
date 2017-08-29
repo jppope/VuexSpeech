571260517fa1559d14ac5a994346aa3ff86b9061
Jon Paul Uritis <jonpaul.act@gmail.com>
2017-08-28 22:31:52 -0700

    added actions, store info

--- rsvp/src/store/store.js
+++ rsvp/src/store/store.js
@@ -7,11 +7,7 @@ Vue.use(Vuex);
 // cause like eslint doesn't like not- export default
 export const store = new Vuex.Store({
   state: {
-    step: 0,
     user: {},
-    rsvp: {
-      attending: null,
-    },
   },
   getters: {
     step: state => state.step,
@@ -19,17 +15,9 @@ export const store = new Vuex.Store({
   mutations: {
     incrementStep: state => state.step++,
     setUser: (state, user) => { state.user = user },
-    responded: state => state.rsvp.responded = true,
-    attending: state => state.rsvp.attending = true,
-    notAttending: state => state.rsvp.attending = false,
-    withGuest: state => state.rsvp.guest = true,
   },
   actions: {
     incrementStep: context => context.commit('incrementStep'),
-    responded: context => context.commit('responded'),
-    attending: context => context.commit('attending'),
-    notAttending: context => context.commit('notAttending'),
-    withGuest: context => context.commit('withGuest'),
     checkUser: (context, payload) => {
       // pretend this is a database call
       if (payload == 'peteruritis@gmail.com') {
